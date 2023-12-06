1e0e4c6c40a9d1d759db8eac9a1771803df2dfc2
Stefan Kaestle <stefan.kaestle@dfinity.org>
2023-12-06 17:54:18 +0100

    Added other calls as well

--- rust/atomic_transactions/src/dex/lib.rs
+++ rust/atomic_transactions/src/dex/lib.rs
@@ -6,9 +6,8 @@ use ic_cdk::api::management_canister::main::{
 use ic_cdk::api::management_canister::provisional::CanisterSettings;
 use ic_cdk_macros::{query, update};
 
+use std::cell::RefCell;
 use std::collections::BTreeMap;
-use std::sync::Arc;
-use std::sync::RwLock;
 
 type CanisterId = Principal;
 type TransactionId = usize;
@@ -21,31 +20,44 @@ pub const WASM: &[u8] =
 
 thread_local! {
     // A list of canister IDs for data partitions
-    static CANISTER_IDS: Arc<RwLock<Vec<Principal>>> = Arc::new(RwLock::new(vec![]));
-    static TRANSACTION_STATE: Arc<RwLock<TransactionList>> = Arc::new(RwLock::new(TransactionList {
-        transaction_number: 0,
+    static CANISTER_IDS: RefCell<Vec<Principal>> = RefCell::new(vec![]);
+    static TRANSACTION_STATE: RefCell<TransactionList> = RefCell::new(TransactionList {
+        next_transaction_number: 0,
         transactions: BTreeMap::new(),
-    }));
+    });
 }
 
-#[derive(CandidType, Debug, Copy, Clone)]
+#[derive(CandidType, Debug, Copy, Clone, PartialEq, Eq)]
 enum TransactionStatus {
+    // Active states
     Preparing,
+    Aborting,
+    Committing,
+    // Final states
     Committed,
-    Failed,
+    Aborted,
 }
 
 struct TransactionList {
-    transaction_number: TransactionId,
+    next_transaction_number: TransactionId,
     transactions: BTreeMap<TransactionId, TransactionState>,
 }
 
 struct TransactionState {
+    total_number_of_children: u64, // Total number of canisters participating in 2PC
+    transaction_status: TransactionStatus,
+    // Prepare
+    pending_prepare_calls: Vec<Call>,
+    num_prepare_okay: u64,
+    num_prepare_fail: u64,
+    // Abort
+    pending_abort_calls: Vec<Call>,
+    num_abort_okay: u64,
+    num_abort_fail: u64,
+    // Commit
+    pending_commit_calls: Vec<Call>,
     num_commit_okay: u64,
     num_commit_fail: u64,
-    total_number_of_children: u64, // Total number of canisters participating in 2PC
-    pending_commits: Vec<Call>,
-    transaction_status: TransactionStatus,
 }
 
 #[derive(CandidType, Debug)]
@@ -54,33 +66,33 @@ struct TransactionResult {
     state: TransactionStatus,
 }
 
+#[derive(Clone)]
 struct Call {
     target: CanisterId,
     method: String,
-    payload: Vec<u8>, // or similar
+    payload: Vec<u8>,
 }
 
 fn get_canister_ids() -> Vec<CanisterId> {
-    CANISTER_IDS.with(|canister_ids| {
-        let canister_ids = canister_ids.read().unwrap();
-        canister_ids.clone()
-    })
+    CANISTER_IDS.with(|canister_ids| canister_ids.borrow().clone())
 }
+
+// https://github.com/dfinity/ic-docutrack/blob/main/backend/src/lib.rs#L222
 
 #[update]
 async fn init() {
     // Create partitions if they don't exist yet
     if CANISTER_IDS.with(|canister_ids| {
-        let canister_ids = canister_ids.read().unwrap();
+        let canister_ids = canister_ids.borrow();
         canister_ids.len() == 0
     }) {
         create_ledgers_from_wasm().await;
     }
 }
 
-fn get_transaction_state(tid: TransactionId) -> TransactionResult {
+fn _get_transaction_state(tid: TransactionId) -> TransactionResult {
     TRANSACTION_STATE.with(|state| {
-        let state = state.read().unwrap();
+        let state = state.borrow();
         let transaction_state = state.transactions.get(&tid).unwrap();
         TransactionResult {
             transaction_number: tid,
@@ -90,60 +102,220 @@ fn get_transaction_state(tid: TransactionId) -> TransactionResult {
 }
 
 #[update]
-async fn start_transaction() -> TransactionResult {
+async fn swap_token1_to_token2() -> TransactionResult {
     TRANSACTION_STATE.with(|state| {
         // Get next possible transaction number
-        let mut state = state.write().unwrap();
-        state.transaction_number += 1;
+        let mut state = state.borrow_mut();
+        state.next_transaction_number += 1;
 
-        let tid = state.transaction_number;
+        let tid = state.next_transaction_number;
+
+        let canisters = get_canister_ids();
+        let canister_id_1 = canisters[0];
+        let canister_id_2 = canisters[1];
 
         state.transactions.insert(
             tid,
             TransactionState {
+                total_number_of_children: get_canister_ids().len() as u64,
+                transaction_status: TransactionStatus::Preparing,
+                // Prepare
+                pending_prepare_calls: vec![
+                    Call {
+                        target: canister_id_1,
+                        method: "prepare_transaction".to_string(),
+                        payload: Encode!(&(tid, 1, -1337,)).unwrap(),
+                    },
+                    Call {
+                        target: canister_id_2,
+                        method: "prepare_transaction".to_string(),
+                        payload: Encode!(&(tid, 2, 42,)).unwrap(),
+                    },
+                ],
+                num_prepare_okay: 0,
+                num_prepare_fail: 0,
+                // Abort
+                pending_abort_calls: vec![
+                    Call {
+                        target: canister_id_1,
+                        method: "abort_transaction".to_string(),
+                        payload: Encode!(&(tid, 1, -1337,)).unwrap(),
+                    },
+                    Call {
+                        target: canister_id_2,
+                        method: "abort_transaction".to_string(),
+                        payload: Encode!(&(tid, 2, 42,)).unwrap(),
+                    },
+                ],
+                num_abort_okay: 0,
+                num_abort_fail: 0,
+                // Commit
+                pending_commit_calls: vec![
+                    Call {
+                        target: canister_id_1,
+                        method: "commit_transaction".to_string(),
+                        payload: Encode!(&(tid, 1, -1337,)).unwrap(),
+                    },
+                    Call {
+                        target: canister_id_2,
+                        method: "commit_transaction".to_string(),
+                        payload: Encode!(&(tid, 2, 42,)).unwrap(),
+                    },
+                ],
                 num_commit_okay: 0,
                 num_commit_fail: 0,
-                total_number_of_children: get_canister_ids().len() as u64,
-                pending_commits: vec![],
-                transaction_status: TransactionStatus::Preparing,
             },
         );
 
-        get_transaction_state(tid)
+        _get_transaction_state(tid)
     })
+}
+
+#[query]
+fn get_transaction_state(tid: TransactionId) -> TransactionResult {
+    _get_transaction_state(tid)
 }
 
 #[update]
 /// Resume executing a transaction.
 ///
 /// Calling this function might change the state of the transaction.
-async fn check_transaction_state(tid: TransactionId) -> TransactionResult {
-    let transaction_state = TRANSACTION_STATE.with(|state| {
-        let mut state = state.write().unwrap();
-        let transaction_state = state.transactions.get_mut(&tid).unwrap();
-        transaction_state
+async fn transaction_loop(tid: TransactionId) -> TransactionResult {
+    let transaction_status = TRANSACTION_STATE.with(|state| {
+        state
+            .borrow()
+            .transactions
+            .get(&tid)
+            .unwrap()
+            .transaction_status
     });
 
-    match transaction_state.transaction_status {
+    match transaction_status {
         TransactionStatus::Preparing => {
+            let pending_prepare_calls = TRANSACTION_STATE.with(|state| {
+                state
+                    .borrow()
+                    .transactions
+                    .get(&tid)
+                    .unwrap()
+                    .pending_prepare_calls
+                    .clone()
+            });
+
             // Trigger all calls that have not been triggered yet
-            for call in transaction_state.pending_commits.iter() {
+            for call in pending_prepare_calls {
                 let call_raw_result =
                     call_raw(call.target, &call.method, call.payload.clone(), 0).await;
 
-                if call_raw_result.is_ok() {
-                    transaction_state.num_commit_okay += 1;
-                } else {
-                    // Should we retry in this case?
-                    transaction_state.num_commit_fail += 1;
-                }
+                TRANSACTION_STATE.with(|state| {
+                    let mut state = state.borrow_mut();
+                    let transaction_state = state.transactions.get_mut(&tid).unwrap();
+                    if call_raw_result.is_ok() {
+                        transaction_state.num_prepare_okay += 1;
+                    } else {
+                        transaction_state.num_prepare_fail += 1;
+                    }
+                });
             }
+
+            TRANSACTION_STATE.with(|state| {
+                let mut state = state.borrow_mut();
+                let transaction_state = state.transactions.get_mut(&tid).unwrap();
+
+                // Change the state of the transaction based on the total number of responses
+                if transaction_state.num_prepare_okay == transaction_state.total_number_of_children
+                    && transaction_state.transaction_status == TransactionStatus::Preparing
+                {
+                    transaction_state.transaction_status = TransactionStatus::Committing;
+                } else if transaction_state.num_prepare_fail > 0 {
+                    transaction_state.transaction_status = TransactionStatus::Aborting;
+                }
+            });
         }
-        TransactionStatus::Committed => todo!(),
-        TransactionStatus::Failed => todo!(),
+        TransactionStatus::Aborting => {
+            let pending_abort_calls = TRANSACTION_STATE.with(|state| {
+                state
+                    .borrow()
+                    .transactions
+                    .get(&tid)
+                    .unwrap()
+                    .pending_abort_calls
+                    .clone()
+            });
+
+            // Trigger all calls that have not been triggered yet
+            for call in pending_abort_calls {
+                let call_raw_result =
+                    call_raw(call.target, &call.method, call.payload.clone(), 0).await;
+
+                TRANSACTION_STATE.with(|state| {
+                    let mut state = state.borrow_mut();
+                    let transaction_state = state.transactions.get_mut(&tid).unwrap();
+                    if call_raw_result.is_ok() {
+                        transaction_state.num_abort_okay += 1;
+                    } else {
+                        transaction_state.num_abort_fail += 1;
+                    }
+                });
+            }
+
+            TRANSACTION_STATE.with(|state| {
+                let mut state = state.borrow_mut();
+                let transaction_state = state.transactions.get_mut(&tid).unwrap();
+
+                // Change the state of the transaction based on the total number of responses
+                if transaction_state.num_abort_okay == transaction_state.total_number_of_children {
+                    transaction_state.transaction_status = TransactionStatus::Aborted;
+                } else if transaction_state.num_abort_fail > 0 {
+                    // No state change here!
+                }
+            });
+        }
+        TransactionStatus::Committing => {
+            let pending_commit_calls = TRANSACTION_STATE.with(|state| {
+                state
+                    .borrow()
+                    .transactions
+                    .get(&tid)
+                    .unwrap()
+                    .pending_commit_calls
+                    .clone()
+            });
+
+            // Trigger all calls that have not been triggered yet
+            for call in pending_commit_calls {
+                let call_raw_result =
+                    call_raw(call.target, &call.method, call.payload.clone(), 0).await;
+
+                TRANSACTION_STATE.with(|state| {
+                    let mut state = state.borrow_mut();
+                    let transaction_state = state.transactions.get_mut(&tid).unwrap();
+                    if call_raw_result.is_ok() {
+                        transaction_state.num_commit_okay += 1;
+                    } else {
+                        transaction_state.num_commit_fail += 1;
+                    }
+                });
+            }
+
+            TRANSACTION_STATE.with(|state| {
+                let mut state = state.borrow_mut();
+                let transaction_state = state.transactions.get_mut(&tid).unwrap();
+
+                // Change the state of the transaction based on the total number of responses
+                if transaction_state.num_commit_okay == transaction_state.total_number_of_children {
+                    transaction_state.transaction_status = TransactionStatus::Committed;
+                } else if transaction_state.num_commit_fail > 0 {
+                    // No state change here!
+                }
+            });
+        }
+        // We are already in a final state, no need to do anything
+        TransactionStatus::Committed => {}
+        TransactionStatus::Aborted => {}
     }
 
-    get_transaction_state(tid)
+    _get_transaction_state(tid)
 }
 
 async fn create_ledgers_from_wasm() {
@@ -171,14 +343,14 @@ async fn create_ledgers_from_wasm() {
 
         install_code(install_args).await.unwrap();
 
-        let token_names: Vec<u32> = vec![i as u32];
-        let token_balances: Vec<u64> = vec![1000];
+        let token_names: Vec<u32> = vec![(i + 1) as u32];
+        let token_balances: Vec<u64> = vec![10000];
         let _: () = ic_cdk::call(canister_id, "init", (token_names, token_balances))
             .await
             .unwrap();
 
         CANISTER_IDS.with(|canister_ids| {
-            let mut canister_ids = canister_ids.write().unwrap();
+            let mut canister_ids = canister_ids.borrow_mut();
             canister_ids.push(canister_id);
         });
     }
--- rust/atomic_transactions/src/ledger_token_1/lib.rs
+++ rust/atomic_transactions/src/ledger_token_1/lib.rs
@@ -1,4 +1,5 @@
-use ic_cdk_macros::update;
+use candid::Principal;
+use ic_cdk_macros::{query, update};
 use ic_stable_structures::{BTreeMap, BoundedStorable, DefaultMemoryImpl, Storable};
 use std::cell::RefCell;
 
@@ -7,18 +8,24 @@ type TokenBalance = u64;
 type TransactionId = u32;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-enum TransactionState {
+struct TransactionState {
+    status: TransactionStatus,
+    owner: Principal,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum TransactionStatus {
     Prepared,
     Aborted,
     Comitted,
 }
 
-impl Storable for TransactionState {
+impl Storable for TransactionStatus {
     fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
         let r: u8 = match self {
-            TransactionState::Prepared => 1,
-            TransactionState::Aborted => 2,
-            TransactionState::Comitted => 3,
+            TransactionStatus::Prepared => 1,
+            TransactionStatus::Aborted => 2,
+            TransactionStatus::Comitted => 3,
         };
         std::borrow::Cow::Owned(vec![r])
     }
@@ -28,15 +35,15 @@ impl Storable for TransactionState {
         let v = unsafe { *ptr };
 
         match v {
-            1 => TransactionState::Prepared,
-            2 => TransactionState::Aborted,
-            3 => TransactionState::Comitted,
+            1 => TransactionStatus::Prepared,
+            2 => TransactionStatus::Aborted,
+            3 => TransactionStatus::Comitted,
             _ => panic!("Unsupported transaction state"),
         }
     }
 }
 
-impl BoundedStorable for TransactionState {
+impl BoundedStorable for TransactionStatus {
     const MAX_SIZE: u32 = 1;
     const IS_FIXED_SIZE: bool = true;
 }
@@ -47,7 +54,7 @@ thread_local! {
         BTreeMap::init(DefaultMemoryImpl::default()));
 
     // Balances of tokens stored in this ledger
-    static PC_STATE: RefCell<BTreeMap<TokenName, TransactionState, DefaultMemoryImpl>> = RefCell::new(
+    static PC_STATE: RefCell<BTreeMap<TokenName, TransactionStatus, DefaultMemoryImpl>> = RefCell::new(
         BTreeMap::init(DefaultMemoryImpl::default()));
 }
 
@@ -57,6 +64,7 @@ fn init(token_names: Vec<TokenName>, token_balances: Vec<TokenBalance>) {
     BALANCES.with_borrow_mut(|balances| {
         for (name, balance) in token_names.iter().zip(token_balances) {
             balances.insert(*name, balance);
+            ic_cdk::println!("Inital token {} with balance {}", name, balance);
         }
     });
 }
@@ -67,24 +75,27 @@ fn init(token_names: Vec<TokenName>, token_balances: Vec<TokenBalance>) {
 /// This code ensures that resource exists and that the change in balance does not create overflows.
 /// It also ensures that the given resource has not already been prepared by another transaction.
 /// If this is okay, response "yes", otherwise "no".
+///
+/// XXX - This is currently not idempotent. For that, we would need to record the principal for which
+/// a previous prepare has been issued.
 fn prepare_transaction(tid: TransactionId, resource: TokenName, balance_change: i64) -> bool {
     ic_cdk::println!("Preparing transaction: {}", tid);
 
     PC_STATE.with_borrow_mut(|pc_state| {
         BALANCES.with_borrow_mut(|balances| {
             match pc_state.get(&resource) {
-                Some(TransactionState::Prepared) => {
+                Some(TransactionStatus::Prepared) => {
                     // Resource already in prepare state, reject further prepare statements.
                     false
                 }
-                Some(TransactionState::Aborted) | Some(TransactionState::Comitted) | None => {
+                Some(TransactionStatus::Aborted) | Some(TransactionStatus::Comitted) | None => {
                     match balances.get(&resource) {
                         Some(resource_balance) => {
                             // Check if given balance exists and if overflow would happen for the given change in balance
                             match resource_balance.checked_add_signed(balance_change) {
                                 Some(_) => {
                                     // Resource not locked in 2PC
-                                    pc_state.insert(resource, TransactionState::Prepared);
+                                    pc_state.insert(resource, TransactionStatus::Prepared);
                                     true
                                 }
                                 None => false,
@@ -103,10 +114,14 @@ fn prepare_transaction(tid: TransactionId, resource: TokenName, balance_change: 
 ///
 /// Resets the state of the given resource to "aborted". This will free up resources that have
 /// previously been locked by responding "yes" to previous "prepare" requests.
+///
+/// Aborting of the transaction is unconditional.
+///
+/// Has to be idempotent.
 fn abort_transaction(tid: TransactionId, resource: TokenName) {
     ic_cdk::println!("Aborting transaction: {}", tid);
     PC_STATE.with_borrow_mut(|pc_state| {
-        pc_state.insert(resource, TransactionState::Aborted);
+        pc_state.insert(resource, TransactionStatus::Aborted);
     })
 }
 
@@ -114,10 +129,12 @@ fn abort_transaction(tid: TransactionId, resource: TokenName) {
 /// Commit changes according to previously prepared balance change and resource.
 ///
 /// If this fails, there is likely a bug in the protocol.
+///
+/// XXX - This is currently not idempotent.
 fn commit_transaction(tid: TransactionId, resource: TokenName, balance_change: i64) {
     ic_cdk::println!("Committing transaction: {}", tid);
     PC_STATE.with_borrow_mut(|pc_state| {
-        assert_eq!(pc_state.get(&resource), Some(TransactionState::Prepared));
+        assert_eq!(pc_state.get(&resource), Some(TransactionStatus::Prepared));
         BALANCES.with_borrow_mut(|balances| {
             balances.insert(
                 resource.clone(),
@@ -127,7 +144,7 @@ fn commit_transaction(tid: TransactionId, resource: TokenName, balance_change: i
                     .checked_add_signed(balance_change)
                     .expect("Token balance overflow - prepare should have failed"),
             );
-            pc_state.insert(resource, TransactionState::Comitted);
+            pc_state.insert(resource, TransactionStatus::Comitted);
         });
     })
 }
