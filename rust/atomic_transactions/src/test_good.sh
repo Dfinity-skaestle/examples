37efd648af1858984c3860d6d611f1fd5a82d27e
Stefan Kaestle <stefan.kaestle@dfinity.org>
2023-12-22 09:25:16 +0100

    Check transaction status in tests

--- rust/atomic_transactions/src/test_good.sh
+++ rust/atomic_transactions/src/test_good.sh
@@ -2,15 +2,13 @@
 
 set -euox pipefail
 
-dfx build dex 
-dfx canister install dex --mode=reinstall -y
+function error() {
+    echo "$1"
+    exit 1
+}
+
 dfx canister call dex init '()'
 
 dfx canister call dex swap_token1_to_token2 '()'
 dfx canister call dex transaction_loop '(0: nat64)'
-
-dfx canister call dex swap_tokens '("ICP", "EUR", -2000: int64, 64: int64)'
-dfx canister call dex transaction_loop '(1: nat64)'
-dfx canister call dex transaction_loop '(1: nat64)'
-
-dfx canister call dex transaction_loop '(0: nat64)'
+dfx canister call dex transaction_loop '(0: nat64)' | grep "Committed" || error "Transaction 0 was not committed"
--- rust/atomic_transactions/src/test_race.sh
+++ rust/atomic_transactions/src/test_race.sh
@@ -2,8 +2,11 @@
 
 set -euox pipefail
 
-dfx build dex 
-dfx canister install dex --mode=reinstall -y
+function error() {
+    echo "$1"
+    exit 1
+}
+
 dfx canister call dex init '()'
 
 dfx canister call dex swap_token1_to_token2 '()'
@@ -11,6 +14,6 @@ dfx canister call dex transaction_loop '(0: nat64)'
 
 dfx canister call dex swap_tokens '("ICP", "EUR", -2000: int64, 64: int64)'
 dfx canister call dex transaction_loop '(1: nat64)'
-dfx canister call dex transaction_loop '(1: nat64)'
+dfx canister call dex transaction_loop '(1: nat64)' | grep "Aborted" || error "Transaction 1 was not aborted"
 
-dfx canister call dex transaction_loop '(0: nat64)'
+dfx canister call dex transaction_loop '(0: nat64)' | grep "Committed" || error "Transaction 0 was not committed"
