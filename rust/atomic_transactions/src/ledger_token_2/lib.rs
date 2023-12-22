4d026511eedd7412eb28f2ba2f389f832af94a3c
Stefan Kaestle <stefan.kaestle@dfinity.org>
2023-12-22 15:08:01 +0100

    Removed unused second ledger

--- rust/atomic_transactions/src/ledger_token_2/lib.rs
+++
@@ -1,8 +0,0 @@
-use ic_cdk_macros::{query, update};
-use ic_stable_structures::{BTreeMap, DefaultMemoryImpl};
-use std::cell::RefCell;
-
-#[update]
-fn swap() {
-    ic_cdk::println!("Swapping");
-}
